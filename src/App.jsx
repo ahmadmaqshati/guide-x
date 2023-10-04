bfb0c20315a64fd26232f571bf8f45fb8e647f58
Ahmad M <ahmadkhaled.dev@gmail.com>
2023-10-04 22:57:14 +0300

    project-starting

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+function App() {
+  /*
+  =========
+  table of contents:
+  1-home 2-about us & contact 3-membership 4-jobs 5-guide
+  ========= 
+   */
+  return (
+    /* --------------------------------home suction----------------------------------*/
+    <div>
+
+      <div style={{ width: '186.71px', height: '45.25px', background: 'red', padding: '22px' }}>
+        <img src="imges/home.png" alt="" style={{ width: '100%', height: '100%' }} />
+      </div>
+
+
+
+
+    </div>
+    /* ///////////////////////////////home suction//////////////////////////////////*/
+
+  )
+}
+
+export default App
